8f0e536fb75e9cdfb65c59420d6e28abf13491d1
Stefani Neves <neves.ste.crato@gmail.com>
2026-03-25 12:05:40 -0300

    Implementação dos padrões Composite e Strategy

--- src/main/java/sorveteria/strategy/PagamentoDinheiro.java
+++ src/main/java/sorveteria/strategy/PagamentoDinheiro.java
@@ -3,6 +3,10 @@ package sorveteria.strategy;
 public class PagamentoDinheiro implements PagamentoStrategy {
     @Override
     public void pagar(double valor) {
+        double valorPago = Math.ceil(valor);
+        double troco = valorPago - valor;
+        System.out.printf("💵 Valor recebido: R$ %.2f%n", valorPago);
+        System.out.printf("🪙 Troco: R$ %.2f%n", troco);
         System.out.printf("✅ Pagamento em Dinheiro de R$ %.2f realizado!%n", valor);
     }
 }
